98051a406b8e1dbba672f9c100d3edacd593e9d1
georges <georges@gremion.ch>
2025-11-25 13:53:11 +0100

    Fix install.sh postgres commands when running as root

--- install.sh
+++ install.sh
@@ -32,9 +32,10 @@ $SUDO apt-get install -y nodejs
 
 echo "==> PostgreSQL installieren/pruefen"
 $SUDO apt-get install -y postgresql postgresql-contrib
-$SUDO -u postgres psql -tc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'" | grep -q 1 || $SUDO -u postgres psql -c "CREATE DATABASE ${DB_NAME};"
-$SUDO -u postgres psql -tc "SELECT 1 FROM pg_roles WHERE rolname='${DB_USER}'" | grep -q 1 || $SUDO -u postgres psql -c "CREATE USER ${DB_USER} WITH ENCRYPTED PASSWORD '${DB_PASS}';"
-$SUDO -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};"
+# Auch als root immer sudo -u postgres nutzen, damit der Benutzer stimmt
+sudo -u postgres psql -tc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'" | grep -q 1 || sudo -u postgres psql -c "CREATE DATABASE ${DB_NAME};"
+sudo -u postgres psql -tc "SELECT 1 FROM pg_roles WHERE rolname='${DB_USER}'" | grep -q 1 || sudo -u postgres psql -c "CREATE USER ${DB_USER} WITH ENCRYPTED PASSWORD '${DB_PASS}';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};"
 
 # Repository nutzen (falls Skript bereits im Repo ausgefuehrt wird, kein erneutes Clonen)
 if [ -f "package.json" ] && grep -q "\"name\": \"physio-app\"" package.json; then
